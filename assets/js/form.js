3977ba1455538fb8809b388c3832c2f1d4ee69cb
yoneison <yoneisonbayona@gmail.com>
2023-12-07 14:17:42 -0500

    first commit

---
+++ assets/js/form.js
@@ -0,0 +1,46 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const dialog = document.getElementById("dialog");
+
+  const showButton = document.getElementById('contactMeBtn');
+
+  const closeButton = document.getElementById("close_button");
+
+  // "Show the dialog" button opens the dialog modally
+  showButton.addEventListener("click", () => {
+    dialog.showModal();
+  });
+
+  // "Close" button closes the dialog
+  closeButton.addEventListener("click", () => {
+    dialog.close();
+  });
+
+
+
+
+  //form -emailJS
+
+  const btnSend = document.getElementById('btn-send');
+  const form = document.getElementById('form')
+
+  btnSend.addEventListener('click', function (e) {
+    e.preventDefault();
+
+
+    const serviceID = 'service_d4whajr';
+    const templateID = 'template_znu4j0b';
+ 
+    emailjs.sendForm(serviceID, templateID, )
+     .then(() => {
+       window.alert('Sent!');
+       form.reset();
+       dialog.close();
+
+     })
+      .catch((error) => {
+        alert(JSON.stringify(error));
+      })
+
+
+  })
+})
